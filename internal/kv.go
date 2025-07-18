6103e504e09137310209fa942c469297c7e54208
Pentti Laitinen <Pentti.Laitinen@teliacygate.fi>
2025-07-18 15:55:04 +0300

    consolidated files for kv operations

--- internal/kv.go
+++ internal/kv.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net/url"
 	"os"
 	"slices"
 	"strconv"
@@ -14,6 +15,8 @@ import (
 	"github.com/Lazah/vault-cli-go/internal/vault"
 	"github.com/spf13/viper"
 )
+
+//from copy file
 
 type CopyParams struct {
 	SrcPath      string
@@ -346,4 +349,284 @@ func sendDataToChan[T any](inputVals []T, targetChan chan T) {
 		targetChan <- val
 	}
 	close(targetChan)
+}
+
+//from move file
+
+func MoveSecrets(inputParams *CopyParams) {
+	logger := slog.Default()
+	var cfg ClientConfig
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		logger.Error("failed to parse config", slog.String("error", err.Error()))
+		os.Exit(10)
+	}
+	if cfg.SrcVault == nil {
+		logger.Error("can't perform operation as vault client config is missing")
+		os.Exit(10)
+	}
+	srcVaultClient, err := initVaultClient(cfg.SrcVault)
+	if err != nil {
+		logger.Error("failed to initialize vault client", slog.String("error", err.Error()))
+		os.Exit(10)
+	}
+	srcVault, err := srcVaultClient.NewKv2Vault(inputParams.SrcMountPath)
+	if err != nil {
+		logger.Error("failed to initialize kv store", slog.String("path", inputParams.SrcMountPath))
+		os.Exit(10)
+	}
+	logger.Info("initializing destination vault client")
+	var dstVaultClient *vault.VaultClient
+	if cfg.DstVault == nil {
+		dstVaultClient, err = initVaultClient(cfg.SrcVault)
+	} else {
+		dstVaultClient, err = initVaultClient(cfg.DstVault)
+	}
+	if err != nil {
+		logger.Error("failed to initialize vault client", slog.String("error", err.Error()))
+		os.Exit(10)
+	}
+	dstVault, err := dstVaultClient.NewKv2Vault(inputParams.DstMountPath)
+	if err != nil {
+		logger.Error("failed to initialize destination vault", slog.String("error", err.Error()))
+		os.Exit(10)
+	}
+	srcPath := strings.Trim(inputParams.SrcPath, "/")
+	srcPathChan := srcVault.GetSecretPaths(srcPath)
+	sourcePaths := make([]string, 0)
+	for path := range srcPathChan {
+		sourcePaths = append(sourcePaths, path)
+	}
+	if len(sourcePaths) == 0 {
+		logger.Info("nothing to move from path", slog.String("path", srcPath))
+		os.Exit(0)
+	}
+	secretPathChan, metadataChan := startMetadataReaders(srcVault, inputParams.Versions)
+	go sendDataToChan(sourcePaths, secretPathChan)
+	secretVersions, err := collectResults(metadataChan)
+	if err != nil {
+		logger.Error(
+			"an error occured while collecting metadata",
+			slog.String("error", err.Error()),
+		)
+		os.Exit(10)
+	}
+	copyChan, failureChan, copierGroup := startSecretCopiers(srcVault, dstVault)
+	dstPath := strings.Trim(inputParams.DstPath, "/")
+	for _, secretVersion := range secretVersions {
+		trimmedPath := strings.TrimPrefix(secretVersion.secretPath, srcPath)
+		versionInfo := &SecretVersionsToCopy{
+			origPath: secretVersion.secretPath,
+			versions: secretVersion.versions,
+			newPath:  fmt.Sprintf("%s%s", dstPath, trimmedPath),
+		}
+		copyChan <- versionInfo
+	}
+	close(copyChan)
+	deleteSecrets := true
+	failedPaths, err := collectResults(failureChan)
+	if err != nil {
+		logger.Error(
+			"an error occured while collecting copy failures",
+			slog.String("error", err.Error()),
+		)
+		deleteSecrets = false
+	}
+	copierGroup.Wait()
+	if deleteSecrets {
+		var removePaths []string
+		removePaths = append(removePaths, sourcePaths...)
+		for _, failedPath := range failedPaths {
+			logger.Info("removing path from delete list", slog.String("path", failedPath))
+			removePaths = slices.DeleteFunc(removePaths, func(path string) bool {
+				return path == failedPath
+			})
+		}
+		logger.Info("starting secret deletion")
+		pathChan, deleteGroup := startDeleteWorkers(srcVault)
+		for _, path := range removePaths {
+			pathChan <- path
+		}
+		close(pathChan)
+		deleteGroup.Wait()
+	}
+	err = srcVaultClient.RevokeToken()
+	if err != nil {
+		logger.Warn("failed to revoke session token for source vault")
+	}
+	err = dstVaultClient.RevokeToken()
+	if err != nil {
+		logger.Warn("failed to revoke session token for destination vault")
+	}
+
+}
+
+//from list file
+
+func ListSecrets() {
+	logger := slog.Default()
+	var cfg ClientConfig
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		logger.Error("an error occured while reading config", slog.String("error", err.Error()))
+		os.Exit(10)
+	}
+	if cfg.SrcVault == nil {
+		logger.Error("source vault info missing... terminating")
+		os.Exit(10)
+	}
+	client, err := vault.NewClient(cfg.SrcVault.BaseURL)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = client.WithUserAuth(cfg.SrcVault.UserCreds.Username, cfg.SrcVault.UserCreds.Password, "")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = client.Authenticate()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	kv2Vault, err := client.NewKv2Vault("secret")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	secretChan := kv2Vault.GetSecretPaths("")
+	paths := make([]string, 0)
+	for result := range secretChan {
+		paths = append(paths, result)
+	}
+
+	fmt.Println("paths:")
+	for _, v := range paths {
+		fmt.Println(v)
+	}
+
+}
+
+//from delete file
+
+type DeleteParams struct {
+	SrcMountPath string
+	SrcPath      string
+}
+
+func DeleteSecrets(inputParams DeleteParams) {
+	logger := slog.Default()
+	var cfg ClientConfig
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		logger.Error("failed to parse config", slog.String("error", err.Error()))
+		os.Exit(10)
+	}
+	if cfg.SrcVault == nil {
+		logger.Error("can't perform operation as vault client config is missing")
+		os.Exit(10)
+	}
+	srcClient, err := initVaultClient(cfg.SrcVault)
+	if err != nil {
+		logger.Error("failed to initialize vault client", slog.String("error", err.Error()))
+		os.Exit(10)
+	}
+	srcVault, err := srcClient.NewKv2Vault(inputParams.SrcMountPath)
+	if err != nil {
+		logger.Error("failed to initialize vault", slog.String("path", inputParams.SrcMountPath))
+		os.Exit(10)
+	}
+	srcPath := strings.Trim(inputParams.SrcPath, "/")
+	srcPathChan := srcVault.GetSecretPaths(srcPath)
+	sourcePaths := make([]string, 0)
+	for path := range srcPathChan {
+		sourcePaths = append(sourcePaths, path)
+	}
+	if len(sourcePaths) == 0 {
+		logger.Info("nothing to delete from path", slog.String("path", srcPath))
+		os.Exit(0)
+	}
+	pathChan, deleteGroup := startDeleteWorkers(srcVault)
+	for _, path := range sourcePaths {
+		pathChan <- path
+	}
+	close(pathChan)
+	deleteGroup.Wait()
+	err = srcClient.RevokeToken()
+	if err != nil {
+		logger.Error("failed to revoke session token")
+	}
+}
+
+func initVaultClient(vaultCfg *VaultInstance) (*vault.VaultClient, error) {
+	vaultUrl, err := url.Parse(vaultCfg.BaseURL)
+	if err != nil {
+		msg := fmt.Errorf("can't initialize vault client with given url: %w", err)
+		return nil, msg
+	}
+	vaultClient, err := vault.NewClient(vaultUrl.String())
+	if err != nil {
+		msg := fmt.Errorf("vault client init failed: %w", err)
+		return nil, msg
+	}
+	if vaultCfg.Insecure {
+		err = vaultClient.Insecure()
+	}
+	if err != nil {
+		msg := fmt.Errorf("failed to set connection to insecure: %w", err)
+		return nil, msg
+	}
+	switch vaultCfg.AuthType {
+	case "userpass":
+		err = vaultClient.WithUserAuth(vaultCfg.UserCreds.Username, vaultCfg.UserCreds.Password, "")
+		if err != nil {
+			msg := fmt.Errorf("vault authentication failed: %w", err)
+			return nil, msg
+		}
+		err = vaultClient.Authenticate()
+		if err != nil {
+			msg := fmt.Errorf("an error occured while performing authentication to vault: %w", err)
+			return nil, msg
+		}
+	case "ldap":
+		err = vaultClient.WithLdapAuth(vaultCfg.UserCreds.Username, vaultCfg.UserCreds.Password, "")
+		if err != nil {
+			msg := fmt.Errorf("vault authentication failed: %w", err)
+			return nil, msg
+		}
+		err = vaultClient.Authenticate()
+		if err != nil {
+			msg := fmt.Errorf("an error occured while performing authentication to vault: %w", err)
+			return nil, msg
+		}
+	case "token":
+		err = vaultClient.WithTokenAuth(vaultCfg.TokenCreds.Token)
+		if err != nil {
+			msg := fmt.Errorf("vault authentication failed: %w", err)
+			return nil, msg
+		}
+	default:
+		return nil, fmt.Errorf("unsupported authentication method specified")
+	}
+	return vaultClient, nil
+}
+
+func startDeleteWorkers(srcVault *vault.Kv2Vault) (chan string, *sync.WaitGroup) {
+	deleteGroup := new(sync.WaitGroup)
+	pathChan := make(chan string, 100)
+	processCount := 4
+	deleteGroup.Add(processCount)
+	for range processCount {
+		go deleteSecrets(srcVault, pathChan, deleteGroup)
+	}
+	return pathChan, deleteGroup
+}
+
+func deleteSecrets(srcVault *vault.Kv2Vault, pathChan chan string, deleteGroup *sync.WaitGroup) {
+	logger := slog.Default()
+	defer deleteGroup.Done()
+	for path := range pathChan {
+		logger.Info("deleting secret", slog.String("path", path))
+		err := srcVault.DeletSecret(path)
+		if err != nil {
+			logger.Error("failed to delete secret", slog.String("path", path))
+		}
+	}
 }
