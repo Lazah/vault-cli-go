a2809decbbb6cacd4ed0be6b04439f99f5599a59
Pentti Laitinen <Pentti.Laitinen@teliacygate.fi>
2025-07-18 09:23:07 +0300

    conolidated some files

--- internal/vault/client.go
+++ internal/vault/client.go
@@ -30,6 +30,50 @@ type passwdAuth struct {
 	password string
 }
 
+type LdapAuthRespBody struct {
+	LeaseId       string         `json:"lease_id,omitempty"`
+	Renewable     bool           `json:"renewable,omitempty"`
+	LeaseDuration int            `json:"lease_duration,omitempty"`
+	Auth          *ldapTokenData `json:"auth,omitempty"`
+}
+type ldapTokenData struct {
+	ClientToken   string            `json:"client_token,omitempty"`
+	Policies      []string          `json:"policies,omitempty"`
+	Metadata      map[string]string `json:"metadata,omitempty"`
+	LeaseDuration int64             `json:"lease_duration,omitempty"`
+	Renewable     bool              `json:"renewable,omitempty"`
+}
+
+type UserpassAuthRespBody struct {
+	RequestId     string `json:"request_id,omitempty"`
+	LeaseId       string `json:"lease_id,omitempty"`
+	Renewable     bool   `json:"renewable"`
+	LeaseDuration int64  `json:"lease_duration,omitempty"`
+	Auth          *userpassAuthData
+}
+
+type userpassAuthData struct {
+	ClientToken   string            `json:"client_token,omitempty"`
+	Accessor      string            `json:"accessor,omitempty"`
+	Policies      []string          `json:"policies,omitempty"`
+	TokenPolicies []string          `json:"token_policies,omitempty"`
+	Metadata      map[string]string `json:"metadata,omitempty"`
+	LeaseDuration int64             `json:"lease_duration,omitempty"`
+	Renewable     bool              `json:"renewable,omitempty"`
+}
+
+type TokenRenewResp struct {
+	Auth *tokenRenewData
+}
+
+type tokenRenewData struct {
+	ClientToken   string `json:"client_token,omitempty"`
+	Policies      []string
+	Metadata      map[string]string
+	LeaseDuration int64 `json:"lease_duration,omitempty"`
+	Renewable     bool
+}
+
 func NewClient(baseUrl string) (*VaultClient, error) {
 	serverUrl, err := url.Parse(baseUrl)
 	if err != nil {
