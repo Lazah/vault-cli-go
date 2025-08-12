a6816b1b8ec7144f3cbed83b6ace623a558a56a5
Pentti Laitinen <pentti.laitinen@gmail.com>
2025-08-12 22:23:05 +0300

    formatted test data generator

--- test/populate_kv2_vault.py
+++ test/populate_kv2_vault.py
@@ -1,50 +1,67 @@
+import json
 import random
 import string
-import requests
-import json
 
-def create_customer_paths(cust_count:int):
-    prefixes = ["serviceAccounts/local/","serviceAccounts/admin/","serviceAccounts/monitor/","serviceAccounts/audit/","serviceAccounts/orch/"]
+import requests
+
+
+def create_customer_paths(cust_count: int) -> list[str]:
+    prefixes = [
+        "serviceAccounts/local/",
+        "serviceAccounts/admin/",
+        "serviceAccounts/monitor/",
+        "serviceAccounts/audit/",
+        "serviceAccounts/orch/",
+    ]
     entries = []
     for prefix in prefixes:
-        for num in range(1,cust_count+1):
+        for num in range(1, cust_count + 1):
             entries.append(f"{prefix}cust{num}")
     return entries
 
-def add_entry_names_to_customers(list:str):
+
+def add_entry_names_to_customers(names: list[str]) -> list[str]:
     entries = []
-    max_entries = random.randint(1,200)
-    for path in list:
-        for num in range(1,max_entries+1):
+    max_entries = random.randint(1, 200)
+    for path in names:
+        for num in range(1, max_entries + 1):
             entries.append(f"{path}/machine{num}")
     return entries
-def create_data_for_entries(entry_paths:list):
+
+
+def create_data_for_entries(entry_paths: list):
     res = dict()
     for path in entry_paths:
-        max_entries = random.randint(1,9)
+        max_entries = random.randint(1, 9)
         pws = []
-        for num in range(0,max_entries):
-            passwd = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
+        for _num in range(0, max_entries):
+            passwd = "".join(random.choices(string.ascii_letters + string.digits, k=20))
             pws.append(passwd)
-        res[path]=pws
+        res[path] = pws
     return res
 
-def write_data_to_vault(entries:dict):
+
+def write_data_to_vault(entries: dict):
     url = "http://localhost:8200/v1/secret/data/"
-    for path,pw_list in entries.items():
+    for path, pw_list in entries.items():
         final_url = f"{url}{path}"
         print(f"creating entries for '{path}'")
         for passwd in pw_list:
             data = dict()
             body = dict()
-            data["passwd"]=passwd
-            body["data"] = data 
+            data["passwd"] = passwd
+            body["data"] = data
             body_json = json.dumps(body)
             token = ""
-            headers = {"Authorization":f"Bearer {token}","Content-Type":"application/json","Accept":"application/json"}
-            resp = requests.post(url=final_url,headers=headers,data=body_json)
+            headers = {
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+            resp = requests.post(url=final_url, headers=headers, data=body_json)
             if not resp.ok:
                 print(f"failed to post entry for '{path}'")
+
 
 if __name__ == "__main__":
     customers = create_customer_paths(100)
